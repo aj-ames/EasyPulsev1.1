a447d44424c351ef2b20b324d3fc4ee91a0358ef
Akash James <akash.james2096@gmail.com>
2017-08-09 16:29:00 +0530

    EasyPulse: Improved with 3 pulse calculation

--- EasyPulse/EasyPulse.ino
+++ EasyPulse/EasyPulse.ino
@@ -1,61 +1,61 @@
 /*
-  AnalogReadSerial
- Reads an analog input on pin 0, prints the result to the serial monitor 
- 
- This example code is in the public domain.
+  Program to calculate BPM using EasyPulse Sensor v1.1
+  This is achieved by calculating 3 peaks of pulses and averaging the values.
  */
 
 int sensorValue = 0;
 bool pulse = false;
-bool newPulse = false;
-long pulseTimer = 0;
-long newpulseTimer = 0;
+bool done = false;
+int peak1 = 0, peak2 = 0, peak3 = 0;
+float bpm = 0;
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  sensorValue = analogRead(A0);
-  delay(5);
-  if(sensorValue > 1000) {
-    pulse = true;
-    pulseTimer = millis();
-  }
-  if(sensorValue < 10 && pulse == true) {
-    while(true) {
-      sensorValue = analogRead(A0);
-      delay(5);
-      if(sensorValue > 1000) {
-        pulse = false;
-        while(true) {
-          sensorValue = analogRead(A0);
-          delay(5);
-          if(sensorValue < 10) {
-            newpulseTimer = millis();
-            newPulse = true;
-            break;
-          }
+  bpmCalculator();
+}
+
+int peakFinder() {
+  while(true) {
+    sensorValue = analogRead(A0);
+    delay(75);
+    if(sensorValue > 1000) {
+      while(true) {
+        sensorValue = analogRead(A0);
+        delay(75);
+        if(sensorValue < 10) {
+          pulse = true;
+          return millis();
         }
-      }
-      if(newPulse) {
-        Serial.println("BPM: ");
-        float bpm = 60.0/((newpulseTimer/1000.0) - (pulseTimer/1000.0));
-        Serial.println(bpm);
-        newPulse = false;
-        break;
       }
     }
   }
 }
 
-void delay_x(uint32_t millis_delay)
-{
-  uint16_t micros_now = (uint16_t)micros();
-
-  while (millis_delay > 0) {
-    if (((uint16_t)micros() - micros_now) >= 1000) {
-      millis_delay--;
-      micros_now += 1000;
-    }
-  }  
+void bpmCalculator() {
+  peak1 = peakFinder();
+  if(pulse) {
+    pulse = false;
+    peak2 = peakFinder();
+  }
+  if(pulse) {
+    pulse = false;
+    peak3 = peakFinder();
+    done = true;
+  }
+  if(done) {
+    //Serial.println("Pulse 1: ");
+    //Serial.println(peak1);
+    //Serial.println("Pulse 2: ");
+    //Serial.println(peak2);
+    //Serial.println("Pulse 3: ");
+    //Serial.println(peak3);
+    float pulse1 = (peak2 - peak1)/1000.0;;
+    float pulse2 = (peak3 - peak2)/1000.0;
+    float avg = (pulse1 + pulse2)/2.0;
+    bpm = 60.0/avg;
+    Serial.println("BPM:");
+    Serial.println(bpm);
+  }
 }
